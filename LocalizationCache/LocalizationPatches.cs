aefe9832323a14c0f5523e2862c5ea4327ab28f4
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2023-08-23 13:30:15 +0200

    fix: differentiate between text files

--- LocalizationCache/LocalizationPatches.cs
+++ LocalizationCache/LocalizationPatches.cs
@@ -1,14 +1,23 @@
+using System;
 using System.Collections.Generic;
 using HarmonyLib;
+using UnityEngine;
 
 namespace LocalizationCache {
     [HarmonyPatch]
     public static class LocalizationPatches {
-        private static Dictionary<string, Dictionary<string, string>> languageTranslations = new Dictionary<string, Dictionary<string, string>>();
+        private static Dictionary<Tuple<string, string>, Dictionary<string, string>> languageTranslations =
+            new Dictionary<Tuple<string, string>, Dictionary<string, string>>();
 
         [HarmonyPatch(typeof(Localization), nameof(Localization.LoadCSV)), HarmonyPrefix, HarmonyPriority(Priority.First)]
-        public static bool SetupLanguage_Prefix(Localization __instance, string language, ref bool __result) {
-            if (!languageTranslations.TryGetValue(language, out Dictionary<string, string> translations)) {
+        public static bool SetupLanguage_Prefix(Localization __instance, TextAsset file, string language, ref bool __result) {
+            if (!file) {
+                return true;
+            }
+
+            Tuple<string, string> key = new Tuple<string, string>(file.name, language);
+
+            if (!languageTranslations.TryGetValue(key, out Dictionary<string, string> translations)) {
                 return true;
             }
 
@@ -18,9 +27,10 @@ namespace LocalizationCache {
         }
 
         [HarmonyPatch(typeof(Localization), nameof(Localization.LoadCSV)), HarmonyPostfix, HarmonyPriority(Priority.Last)]
-        public static void SetupLanguage_Postfix(Localization __instance, string language, bool __result) {
-            if (__result) {
-                languageTranslations[language] = __instance.m_translations;
+        public static void SetupLanguage_Postfix(Localization __instance, TextAsset file, string language, bool __result) {
+            if (__result && file) {
+                Tuple<string, string> key = new Tuple<string, string>(file.name, language);
+                languageTranslations[key] = __instance.m_translations;
             }
         }
     }
